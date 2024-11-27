fccd7b819e79f50e0b18f8f154b84823bcb2ff9b
EL HAMIDI Nour <156805422+nourlhmd@users.noreply.github.com>
2024-11-27 14:27:28 +0100

    merge sur le master

---
+++ App.java
@@ -0,0 +1,15 @@
+/* point d'entrée principal de ton application Java
+initialise l'interface utilisateur et configure les 
+scènes pour naviguer entre les différentes fenêtres
+*/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class AuctionApp {
+
+    public static void main(String[] args) {} 
+
+}
